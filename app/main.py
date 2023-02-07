4889fc6dcec9ad7e976bf02c1dbda9689189551f
Cyman <simon.maier@stud-mail.uni-wuerzburg.de>
2023-02-07 22:57:48 +0100

    postkarte ausgeben lassen v1

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 # Licensed under the EUPL-1.2-or-later
 
 from postcardscanner import PostcardScanner
-from postcardscanner.hardware.scanner_v2 import ScannerV2
+from scanner.scanner_v3 import ScannerV3
 
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
@@ -16,7 +16,7 @@ def callback(image):
     with open('img.jpg','wb') as out:
         out.write(image.read())
 
-scanner = PostcardScanner(scanner=ScannerV2(callback=callback))
+scanner = PostcardScanner(scanner=ScannerV3(callback=callback))
 scanner.start()
 
 
@@ -44,3 +44,13 @@ async def root():
 @app.get("/last_postcard_timestamp")
 async def root():
     return os.path.getctime('img.jpg')
+
+@app.get("/reject_postcard")
+async def root():
+    scanner.scanner.rejectPostcard()
+    return True
+
+@app.get("/accept_postcard")
+async def root():
+    scanner.scanner.acceptPostcard()
+    return True
